833ba5d4ae97ff3ebf8088113229f804c56d82b3
rsh1994 <rsh1994@naver.com>
2022-05-18 03:47:35 +0900

    Feat: Flask job CRUD구현

--- app.py
+++ app.py
@@ -6,46 +6,76 @@ from flask_restx import Api, resource
 
 app = Flask(__name__)
 
-
-
-@app.route('/')
-def greeting():
-    return "This is Test API!"
+"""
+류성훈
+"""
 
 @app.route('/job', methods=['GET','POST'])
 def post_job():
     
+    # Job file 조회
     if request.method == 'GET':
         with open('job.json','r') as f:
             json_data = json.load(f)
         print(json.dumps(json_data, indent="\t"))
         return json_data
 
+    # Job file 저장
     elif request.method == 'POST':
+        data = json.loads(request.data)
+        
+        with open('job.json', 'r') as f:
+            job_file = json.load(f)
+    
+        job_file[data['job_id']] = {
+                                "job_name": data['job_name'],
+                                "task_list": data['task_list'],
+                                "property": data['property']
+                                 }                
 
+        with open("job.json", "w") as json_file:
+            json.dump(job_file, json_file, indent="\t")
+
+        return job_file
+    
+@app.route('/job/<string:id>', methods=['DELETE','PUT'])
+def update_delete_job(id):
+
+    # 해당 job_id 데이터 삭제
+    if request.method == 'DELETE':
+        with open('job.json', 'r') as f:
+            job_file = json.load(f)
+            
+        if id in job_file.keys():
+            del(job_file[id])
+        else:
+            print("삭제하려는 데이터의 job_id가 존재하지 않습니다.")
+        
+        with open("job.json", "w") as json_file:
+            json.dump(job_file, json_file, indent="\t")
+
+        return job_file
+    
+    # 해당 job_id 데이터 수정
+    elif request.method == 'PUT':
         data = json.loads(request.data)
 
-        with open('job.json','r') as json_file:
-            json_data = json.load(json_file)
+        with open('job.json', 'r') as f:
+            job_file = json.load(f)
 
-        json_data.append(data)
+        if id in job_file.keys():
+            job_file[id] = {
+                            "job_name": data['job_name'],
+                            "task_list": data['task_list'],
+                            "property": data['property']
+                            }
+        else:
+            print("수정하려는 데이터의 job_id가 존재하지 않습니다.")
 
-        with open('job.json', 'w', encoding='utf-8') as make_file:
-            json.dump(json_data, make_file, indent='\t')
+        with open("job.json", "w") as json_file:
+            json.dump(job_file, json_file, indent="\t")
 
-        with open('job.json', 'r') as json_file:
-            json_data = json.load(json_file)
-        print(json.dumps(json_data, indent="\t"))
-        return jsonify(json_data)
-    
-"""
-data = {
-    “job_id”: 1, 
-    “job_name”: “Job1”,
-    “task_list”: {"read": [“drop"], “drop":["write"], “write”:[]},
-    “property”: {“read”: {“task_name”: “read”, “filename” : “path/to/a.csv”, “sep” :“,”}, “drop” : {“task_name”: “drop”, “column_name”: “date”}, “write” : {“task_name”: “write”, “filename” : “path/to/b.csv”, “sep”: “,”}
-    }
-"""
+        return job_file[id]
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0', port=80)
